2fe29f566cee2b9dd3692533d3f93216435b54d2
LukeGorgadze <just.luke.gorgadze@gmail.com>
2022-03-06 02:14:57 +0400

    Update

---
+++ AddingIntegers-8.asm
@@ -0,0 +1,13 @@
+.data
+	number1: .word 5
+	number2: .word 10
+.text
+	lw $t0, number1
+	lw $t1, number2
+	
+	add $t2, $t0, $t1
+	
+	li $v0, 1
+	add $a0, $zero, $t2
+	syscall
+	
--- Integer-5.asm
+++ Integer-5.asm
@@ -3,6 +3,7 @@
 .text
 	li $v0, 1
 	lw $a0, age
+	
 	syscall
 	
 	
